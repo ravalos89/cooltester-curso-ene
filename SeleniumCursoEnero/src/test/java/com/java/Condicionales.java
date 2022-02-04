ca9267344c9b03ea4e0df0242f75f4d25a367977
Ricardo Avalos <ricardo.avalos@automationsensei.com>
2022-02-04 16:21:24 -0600

    commit Ricardo

---
+++ SeleniumCursoEnero/src/test/java/com/java/Condicionales.java
@@ -0,0 +1,20 @@
+package com.java;
+
+public class Condicionales {
+
+	public static void main(String[] args) {
+		// Automovil conduciento a exceso de velocidad
+		
+		// IF
+		
+		// IF ELSE
+		
+		// IF ANIDADO
+		
+		// IF ELSE IF
+		
+		// SWITCH CASE
+
+	}
+
+}
