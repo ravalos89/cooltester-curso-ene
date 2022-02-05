3227c3fbc6ef3279cbb42feedd0cbf5c57ca6bbd
Ricardo Avalos <ricardo.avalos@automationsensei.com>
2022-02-05 11:56:28 -0600

    codigo

---
+++ SeleniumCursoEnero/src/test/java/com/java/Main.java
@@ -0,0 +1,19 @@
+package com.java;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Perro
+		Perro chihuahua = new Perro();
+		chihuahua.raza = "Cabeza de venado";
+		chihuahua.color = "cafe";
+		chihuahua.tamano = "chico";
+		chihuahua.nombre = "Mero Macho";
+		chihuahua.numeroPatas = 4;
+		
+		
+
+	}
+
+}
---
+++ SeleniumCursoEnero/src/test/java/com/java/Perro.java
@@ -0,0 +1,12 @@
+package com.java;
+
+public class Perro {
+	
+	String raza;
+	String color;
+	String tamano;
+	String nombre;
+	int numeroPatas;
+	
+
+}
